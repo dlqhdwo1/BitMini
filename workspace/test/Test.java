da2de19d2d8fc6b70f81257d94fa3a8bb6b4741f
dlqhdwo1 <star5436@naver.com>
2023-03-27 20:48:22 +0900

    test
    test

---
+++ workspace/test/Test.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a= 5;
+		int b = 3;
+		int c =13;
+		
+		int t1,t2,t3;
+		
+		t1 = a&b;
+		t2 = a|b;
+		
+		
+		System.out.println(t1 + "," + t2);
+
+	}
+
+}
